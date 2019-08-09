b7fcc082fb412b27e7a3921e97cba772f9110440
karel <karel@zavadil.eu>
2019-08-09 18:31:45 +0200

    fiddling with database

--- scrum-poker/app/Presenters/HomepagePresenter.php
+++ scrum-poker/app/Presenters/HomepagePresenter.php
@@ -5,8 +5,22 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use App\DatabaseCreator;
 
+final class HomepagePresenter extends Nette\Application\UI\Presenter {
 
-final class HomepagePresenter extends Nette\Application\UI\Presenter
-{
+	/** @var App\DatabaseCreator */
+	private $dbcreator;
+
+	public function __construct(\App\DatabaseCreator $dbcreator) {
+		$this->dbcreator = $dbcreator;
+	}
+	
+	public function startup() {
+		parent::startup();
+		if ($this->dbcreator->createdb()) {
+			$this->flashMessage('database was created.');
+		}
+	}
+
 }
